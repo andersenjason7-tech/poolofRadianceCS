385a01189f40e9d243135656ecad30e2e02b7843
Jason Andersen <andersen.jason7@gmail.com>
2026-03-01 09:12:58 -0700

    add ability to add custom party

--- src/Program.cs
+++ src/Program.cs
@@ -85,7 +85,25 @@ namespace PoolOfRadiance
 
             // Create a party using PartyManagement
             var partyManager = new PartyManagement("Phlan Adventurers");
-            partyManager.CreateDefaultParty();
+
+            // let user choose how the party is created
+            Console.WriteLine("\nChoose party creation method:");
+            Console.WriteLine("[1] Default party");
+            Console.WriteLine("[2] Custom party (build your own character list)");
+            char partyChoice;
+            do
+            {
+                partyChoice = Console.ReadKey(true).KeyChar;
+            } while (partyChoice != '1' && partyChoice != '2');
+
+            if (partyChoice == '1')
+            {
+                partyManager.CreateDefaultParty();
+            }
+            else
+            {
+                CreateCustomParty(partyManager);
+            }
 
             Console.WriteLine("\nYour party is ready to begin their adventure!");
             Console.WriteLine("\nPress any key to enter the town of Phlan...");
@@ -110,6 +128,68 @@ namespace PoolOfRadiance
             Console.WriteLine("\n\nThank you for playing Pool of Radiance!");
             Console.WriteLine("\nPress any key to exit...");
             Console.ReadKey();
+        }
+
+        static void CreateCustomParty(PartyManagement partyManager)
+        {
+            Console.WriteLine("\n=== CUSTOM PARTY CREATION ===\n");
+
+            // ask how many characters
+            int count;
+            do
+            {
+                Console.Write("How many characters in your party (2-8)? ");
+                if (!int.TryParse(Console.ReadLine(), out count)) count = 0;
+            } while (count < 2 || count > 8);
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"\nCreating character {i + 1} of {count}");
+                Console.Write("Name: ");
+                var name = Console.ReadLine() ?? "Unnamed";
+
+                // choose race
+                Console.WriteLine("Choose race:");
+                var races = Enum.GetValues(typeof(PoolOfRadiance.Characters.CharacterRace));
+                for (int r = 0; r < races.Length; r++)
+                {
+                    Console.WriteLine($"[{r + 1}] {races.GetValue(r)}");
+                }
+                int raceIndex;
+                do
+                {
+                    var input = Console.ReadLine();
+                    if (!int.TryParse(input, out raceIndex)) raceIndex = 0;
+                } while (raceIndex < 1 || raceIndex > races.Length);
+                var race = (PoolOfRadiance.Characters.CharacterRace)races.GetValue(raceIndex - 1)!;
+
+                // choose class
+                Console.WriteLine("Choose class:");
+                var classes = PoolOfRadiance.Characters.CharacterClass.Values;
+                int cnum = 1;
+                foreach (var cls in classes)
+                {
+                    Console.WriteLine($"[{cnum}] {cls}");
+                    cnum++;
+                }
+                int classIndex;
+                do
+                {
+                    var input = Console.ReadLine();
+                    if (!int.TryParse(input, out classIndex)) classIndex = 0;
+                } while (classIndex < 1 || classIndex > cnum - 1);
+                var charClass = System.Linq.Enumerable.ElementAt(classes, classIndex - 1);
+
+                var character = new PoolOfRadiance.Characters.Character(name, race, charClass);
+                character.RollStats();
+                // set some default HP based on class hit die
+                character.HitPointsMax = 5 + character.Stats.GetHPModifier();
+                if (character.HitPointsMax < 1) character.HitPointsMax = 1;
+                character.HitPointsCurrent = character.HitPointsMax;
+                partyManager.AddCharacter(character);
+            }
+
+            Console.WriteLine("\nYour custom party is ready!\n");
         }
 
         static void RunQuickDemo()
